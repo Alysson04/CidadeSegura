53b01fa413e8bc85b6f6f13acdb04e58b9d1434e
Alysson Rafael <alysson2rafael@gmail.com>
2024-06-05 19:57:57 -0300

    update

--- src/routes/Inicio/Inicio.jsx
+++ src/routes/Inicio/Inicio.jsx
@@ -3,6 +3,10 @@ import './Inicio.css';
 
 import Navbar from '../../assets/components/Navbar/Navbar';
 import imagem1 from '../../assets/img/imagem1.jpg'
+import imagem2 from '../../assets/img/imagem2.jpg'
+import imagem3 from '../../assets/img/imagem3.jpg'
+import imagem4 from '../../assets/img/imagem4.jpg'
+
 
 
 const Home = () => {
@@ -25,12 +29,19 @@ return (
   <div className="esquerda">
     
   <img src={imagem1} className="imagem1"></img>
-  <h4>Com o Cidade Segura, monitore incidentes em tempo real e ajude a tornar sua cidade um lugar mais seguro para todos.</h4>
+  <p className="t1">Com o Cidade Segura,<br></br> monitore incidentes em<br></br> tempo real e ajude a tornar<br></br> sua cidade um lugar mais<br></br> seguro para todos.</p>
+  <img src={imagem2} className="imagem2"></img>
+  <p className="t2">Cidade Segura transforma<br></br> sua cidade em uma rede colaborativa de segurança, monitorando e notificando incidentes para proteger<br></br> o bem-estar de todos.</p>
+  
 
       
     </div>
     <div className="meio"></div>
     <div className="direita">
+      <p className="t3">Cidade Segura: a tecnologia<br></br> a serviço da segurança<br></br> urbana, permitindo que você<br></br> reporte e acompanhe eventos críticos com facilidade.</p>
+      <img src={imagem3} className="imagem3"></img>
+      <p className="t4">Garanta uma resposta rápida e eficiente a incidentes na <br></br>sua cidade com o Cidade Segura,<br></br> o app de monitoramento que conecta cidadãos e autoridades.</p>
+      <img src={imagem4} className="imagem4"></img>
 
     </div>
     </div>
